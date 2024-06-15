5080530c246c1f1d8272c60df0ee8e33af082748
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-06-15 16:05:02 -0600

    Register

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { login, forgotPassword, resetPassword, updatePassword } = require('../controllers/authController');
+const { login, register, forgotPassword, resetPassword, updatePassword } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/register', register);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 router.post('/update-password', updatePassword);
